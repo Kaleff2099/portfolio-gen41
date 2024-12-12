3481c5613998c0671a5b99136555ef70b3b0bd12
DanOrnelas <10264842+Kaleff2099@users.noreply.github.com>
2024-12-11 23:46:27 -0600

    final proyect

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+import activeMenu from "./components/active_menu.js";
+import parallax from "./components/parallax.js";
+import loader from "./helpers/loader.js";
+import resetToHome from "./helpers/reload_home.js";
+import sendEmail from "./helpers/send_email.js";
+import updateCopyRightYear from "./helpers/update_year.js";
+
+loader()
+parallax()
+activeMenu()
+resetToHome()
+sendEmail()
+updateCopyRightYear()
